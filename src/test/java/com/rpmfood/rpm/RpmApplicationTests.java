8e7917012201cb816c495bd0b8ec57a391ae4c95
DESKTOP-VNLLQOI\papon <papon9@hotmail.com>
2022-07-04 11:27:08 +0700

    first commit

---
+++ src/test/java/com/rpmfood/rpm/RpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpmfood.rpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
